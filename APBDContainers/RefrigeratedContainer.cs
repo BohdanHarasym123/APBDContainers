4014c3f852014b1b9c5cafae414cf27f99092de1
s31539.haras <s31539@pjwstk.edu.pl>
2025-03-22 17:38:52 +0100

    Added RefrigeratedContainer class

---
+++ APBDContainers/RefrigeratedContainer.cs
@@ -0,0 +1,30 @@
+﻿namespace APBDContainers;
+
+public class RefrigeratedContainer : Container
+{
+    public string Type { get; set; }
+    public double Temperature { get; set; }
+
+    public RefrigeratedContainer(double _TareWeight, double _Height, double _Depth, double _MaximumPayload, string _Type, double _Temperature)
+        : base(_TareWeight, _Height, _Depth, _MaximumPayload)
+    {
+        Type = _Type;
+        Temperature = _Temperature;
+        SerialNumber = $"KON-R-{ContainerCount}";
+    }
+
+    public override void LoadCargo(double mass)
+    {
+        if (mass > MaximumPayload)
+        {
+            throw new OverfillException("Exceeded maximum payload");
+        }
+        
+        CargoMass = mass;
+    }
+
+    public override void EmptyCargo()
+    {
+        CargoMass = 0;
+    }
+}
