53e4471e2141e6f862a27f910cebc8f71a2ec73f
s31539.haras <s31539@pjwstk.edu.pl>
2025-03-22 17:09:48 +0100

    Added class GasContainer

---
+++ APBDContainers/GasContainer.cs
@@ -0,0 +1,33 @@
+﻿namespace APBDContainers;
+
+public class GasContainer : Container, IHazardNotifier
+{
+    public double Pressure { get; set; }
+
+    public GasContainer(double _TareWeight, double _Height, double _Depth, double _MaximumPayload, double _Pressure)
+        : base(_TareWeight, _Height, _Depth, _MaximumPayload)
+    {
+        Pressure = _Pressure;
+        SerialNumber = $"KON-G-{ContainerCount}";
+    }
+
+    public override void LoadCargo(double mass)
+    {
+        if (CargoMass + mass > MaximumPayload)
+        {
+            throw new OverfillException("Exceeded maximum payload");
+        }
+
+        CargoMass += mass;
+    }
+
+    public override void EmptyCargo()
+    {
+        CargoMass = 0.05 * CargoMass;
+    }
+
+    public void notifyHazard(string msg)
+    {
+        Console.WriteLine($"WARNING: Hazardous situation occuring at {SerialNumber} container");
+    }
+}
